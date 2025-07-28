2cc5cca20b8b94a30a477eb4aef9aaab601399c8
phantomz888 <chunnyzxd001@gmail.com>
2025-07-28 23:40:12 +0700

    Update vn.lua

--- vn.lua
+++ vn.lua
@@ -120,7 +120,7 @@ getgenv().ConfigsKaitun = {
 			},
 			["Doing"] = {
 				Minimum_Money = 100_000, -- minimum money to start play this event
-				First_Upgrade_Tree = 7,
+				First_Upgrade_Tree = 1,
 				Maximum_Chi = 400,
 			}
 		},
@@ -147,7 +147,7 @@ getgenv().ConfigsKaitun = {
 			"Koi",
 			--"Soft Sunshine",
 			--"Sakura Bush",
-			"Raiju",
+			--"Raiju",
 		},
 		Start_Do_Honey = 2_000_000 -- start trade fruit for honey at money
 	},
